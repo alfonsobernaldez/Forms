cd678b171191438b5052cd76e29ea81139cfae3f
Alfonso <berni365@hotmail.com>
2020-01-23 14:37:30 +0100

    Basic forms in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Persona } from './interface/persona';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Formularios';
+  personita: Persona = {nombre: '', apellidos: '', poblacion: ''};
+
+  submit(){
+
+    console.log(this.personita);
+  }
+
 }
